8d2401b053ea65c931d978ffc6fbd2d072d2f494
Aswathyvariathuparambil <160355760+Aswathyvariathuparambil@users.noreply.github.com>
2025-12-04 11:09:17 +0530

    Add files via upload

---
+++ vehicle_details.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="vehicle_details.aspx.cs" Inherits="vehicle_registration.vehicle_details" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ vehicle_details.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace vehicle_registration
+{
+    public partial class vehicle_details : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        [WebMethod]
+        public static int Save(string number1, string type1, string year1,string model1)
+        {
+            {
+                #region dbsave
+
+                int retVal = 0;
+                string connectionString = ConfigurationManager.ConnectionStrings["constring"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    // Updated query to include gender
+
+                    string ren = "INSERT INTO vehicle_details (number,type,year,model) output INSERTED.id VALUES('" + number1 + "', '" + type1 + "', '" + year1 + "','" + model1 + "')";
+
+                    using (SqlCommand cmd = new SqlCommand(ren, con))
+                    {
+                        con.Open();
+                        retVal = (int)cmd.ExecuteScalar();
+                        con.Close();
+                    }
+                }
+                #endregion
+                return retVal;
+            }
+        }
+
+
+
+    }
+}
